9af16e2ae91b807aea287746a769b032592833a5
hungtm0912 <hungtm@student.apsce.net>
2023-02-10 11:23:06 +0700

    chỉnh màu chữ footer 2

--- index.php
+++ index.php
@@ -220,7 +220,7 @@ require_once './database/config.php';
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4" style="color: white">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">
               Chuyên Sản Phẩm
@@ -241,7 +241,7 @@ require_once './database/config.php';
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4" style="color: white">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">
               Thông tin  
@@ -262,7 +262,7 @@ require_once './database/config.php';
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4" style="color: white">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">
               Thông Tin Liên hệ
