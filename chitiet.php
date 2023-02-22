8f5aa882b43b548e145a0e6b2bef0e4ca8cefb49
hungtm0912 <hungtm@student.apsce.net>
2023-02-22 09:49:28 +0700

    sửa bảng giá

--- chitiet.php
+++ chitiet.php
@@ -197,9 +197,9 @@
             <?php } ?>
             <?php if ($product['product_sale'] > 0) { ?>
             <span style="margin-left: 20px; font-size: 30px" class="card-text new-price">
-            <?php echo number_format($product['product_price'] - $product['product_price'] * $product['product_sale'] / 100)?>VND</span>
+            <?php echo number_format($product['product_price'] - $product['product_price'] * $product['product_sale'] / 100)?> VNĐ</span>
             <?php } else { ?>
-            <span class="card-text new-price"> <?php echo $product['product_price'] ?> VND</span>
+            <span class="card-text new-price"><?php echo number_format($row_product['product_price'] - ($row_product['product_sale'] / 100 * $row_product['product_price'])) ?> VNĐ</span>
             <?php } ?>
             <p style="font-size: 18px; color: #fff;margin-bottom:0;font-weight: 600;">Mô Tả</p>
             <p class="product-desc"><?php echo $product['product_description'] ?></p>
