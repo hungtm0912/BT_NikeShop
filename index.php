3c1a33bb8c8d0271abedc55f403e22e285244596
hungtm0912 <hungtm@student.apsce.net>
2023-02-22 11:04:29 +0700

    sua lai

--- index.php
+++ index.php
@@ -127,8 +127,10 @@ require_once './database/config.php';
               <?php } else { ?>
               <span class="ico-sale"></span>
               <?php } ?>
-             <a  href="./chitiet.php?id=<?php echo $product['product_id'] ?>"><img class="card-img-top link" style=" width: 100%; height: 250px;" src="./Admin_view/upload/<?php echo $product['product_image'] ?>"
-                alt="Card image cap"></a>
+
+              <img class="card-img-top link" style=" width: 100%; height: 250px;" src="./Admin_view/upload/<?php echo $product['product_image'] ?>"
+                alt="Card image cap">
+
               <div style="padding: 6px !important;" class="card-body">
                 <a class="card-title product-title"
                   href="./chitiet.php?id=<?php echo $product['product_id'] ?>" style="text-align: center"><?php echo $product['product_name'] ?></a>
