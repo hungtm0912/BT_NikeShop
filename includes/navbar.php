3c54119dff6f14f31b571cccd76d5eed3027d951
Ngô Quang Huy <finnofme7972@gmail.com>
2023-02-28 09:29:02 +0700

    update menu

--- includes/navbar.php
+++ includes/navbar.php
@@ -102,7 +102,77 @@
       </li>
       <li class="nav-item">
         <a class="nav-link active nav-link-active-custom" href="./index.php">Tất Cả Sản Phẩm</a>
+        <div class="subnav-content">
+        <a href="#bring">Bring</a>
+        <a href="#deliver">Deliver</a>
+        <a href="#package">Package</a>
+        <a href="#express">Express</a>
+    </div>
       </li>
+      <style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333; 
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.subnav {
+  float: left;
+  overflow: hidden;
+}
+
+.subnav .subnavbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .subnav:hover .subnavbtn {
+  background-color: red;
+}
+
+.subnav-content {
+  display: none;
+  position: absolute;
+  left: 0;
+  background-color: red;
+  width: 100%;
+  z-index: 1;
+}
+
+.subnav-content a {
+  float: left;
+  color: white;
+  text-decoration: none;
+}
+
+.subnav-content a:hover {
+  background-color: #eee;
+  color: black;
+}
+
+.subnav:hover .subnav-content {
+  display: block;
+}
+</style>
       <li class="nav-item">
         <a class="nav-link active nav-link-active-custom" href="#">Hướng Dẫn Mua Hàng</a>
       </li>
